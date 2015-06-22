15aa94dc1901bd9e7fe3c5ca0d7a45d6eaf547a0
JinQian <osexp2003@gmail.com>
2015-06-22 19:23:18 +0900

    temp

--- proxy-login-automator.js
+++ proxy-login-automator.js
@@ -1,125 +1,190 @@
 'use strict';
-
-//convert `--key value` to cfg[key]=value
-var cfg = process.argv.slice(2/*skip ["node", "xxx.js"]*/).reduce(function (cfg, arg, i, argv) {
-    i % 2 === 0 && arg.slice(0, 2) === '--' && (cfg[arg.slice(2)] = argv[i + 1]);
-    return cfg;
-}, /*init cfg:*/ {host: 'localhost', port: 8080, remote_host: 8080});
-
-if (!cfg.remote_host || !cfg.usr || !cfg.pwd)
-    return console.error('Usage of parameters:\n'
-        + '\t[--host host]\t\t' + 'listening address. Default: localhost. (* means all interfaces)\n'
-        + '\t[--port port]\t\t' + 'listening port. Default: 8080\n'
-        + '\t<--remote_host host>\t\t' + 'real proxy server address\n'
-        + '\t[--remote_port port]\t\t' + 'real proxy server port. Default: 8080\n'
-        + '\t[--pac urlPath]\t\t' + 'proxy_auto_config_file_url_path\n'
-        + '\t[--usr user]\t\t' + 'proxy user id\n'
-        + '\t[--pwd password]\t\t' + 'proxy user password\n'
-    );
-console.error('Using parameters:\n' + JSON.stringify(cfg, null, '  '));
-cfg.buf_proxy_basic_auth = new Buffer('Proxy-Authorization: Basic ' + new Buffer(cfg.usr + ':' + cfg.pwd).toString('base64'));
-
-var CR = 0xd, LF = 0xa, BUF_CR_LF = new Buffer([0xd, 0xa]), BUF_LF = new Buffer([0xa]), BUF_CR = new Buffer([0xd]), BUF_CR_LF_CR_LF = new Buffer([0xd, 0xa, 0xd, 0xa]), BUF_LF_LF = new Buffer([0xa, 0xa]);
-var STATE_NONE = 0, STATE_FOUND_LF = 1, STATE_FOUND_LF_CR = 2;
-
-var net = require('net'), t;
+var net = require('net');
 var HTTPParser = process.binding('http_parser').HTTPParser;
 
-net.createServer({allowHalfOpen: true}, function (socket) {
-    var realCon = net.connect({port: cfg.remote_port, host: cfg.remote_host, allowHalfOpen: true});
-    realCon.on('data', function (buf) {
-        //console.log('<<<<' + (t=new Date()) + '.' + t.getMilliseconds() + '\n' + buf.toString('ascii'));
-        socket.write(buf);
-    }).on('end', function () {
-        socket.end();
-    }).on('close', function () {
-        socket.end();
-    }).on('error', dummy);
+function main() {
+//convert `--key value` to cfg[key]=value
+    var cfg = process.argv.slice(2/*skip ["node", "xxx.js"]*/).reduce(function (cfg, arg, i, argv) {
+        i % 2 === 0 && arg.slice(0, 2) === '--' && (cfg[arg.slice(2)] = argv[i + 1]);
+        return cfg;
+    }, /*init cfg:*/ {local_host: 'localhost', local_port: 8080, remote_host: 8080});
+    cfg.local_port = Number(cfg.local_port);
+    cfg.remote_port = Number(cfg.remote_port);
 
-    var parser = new HTTPParser(HTTPParser.REQUEST);
-    parser[HTTPParser.kOnHeadersComplete] = function () {
-        //console.log('---- kOnHeadersComplete----');
-        //console.log(arguments);
-        parser.__is_headers_complete = true;
-    };
-    //parser[HTTPParser.kOnMessageComplete] = function () {
-    //    console.log('---- kOnMessageComplete----');
-    //    console.log(arguments);
-    //};
+    if (!cfg.local_port || !cfg.remote_host || !cfg.usr || !cfg.pwd)
+        return console.error('Usage of parameters:\n'
+            + '\t[--local_host host]\t\t' + 'listening address. Default: localhost. (* means all interfaces)\n'
+            + '\t[--local_port port]\t\t' + 'listening port. Default: 8080\n'
+            + '\t<--remote_host host>\t\t' + 'real proxy server address\n'
+            + '\t[--remote_port port]\t\t' + 'real proxy server port. Default: 8080\n'
+            + '\t[--pac_url]\t\t' + 'URL of pac (proxy auto configuration script)\n'
+            + '\t[--usr user]\t\t' + 'proxy user id\n'
+            + '\t[--pwd password]\t\t' + 'proxy user password\n'
+        );
+    console.error('Using parameters:\n' + JSON.stringify(cfg, null, '  '));
+    cfg.buf_proxy_basic_auth = new Buffer('Proxy-Authorization: Basic ' + new Buffer(cfg.usr + ':' + cfg.pwd).toString('base64'));
 
-    var state = STATE_NONE;
+    if (cfg.pac_url) {
+        createPacServer(cfg.local_host, cfg.local_port, cfg.remote_host, cfg.remote_port, cfg.buf_proxy_basic_auth, cfg.pac_url);
+    } else {
+        createPortForwarder(cfg.local_host, cfg.local_port, cfg.remote_host, cfg.remote_port, cfg.buf_proxy_basic_auth);
+    }
+}
 
-    socket.on('data', function (buf) {
-        //console.log('>>>>' + (t = new Date()) + '.' + t.getMilliseconds() + '\n' + buf.toString('ascii'));
-        //var ret = parser.execute(buf);
-        //console.log('\n\n----parser result: ' + ret + ' buf len:' + buf.length);
-        //realCon.write(buf);
-        //return;
+var CR = 0xd, LF = 0xa, BUF_CR = new Buffer([0xd]), BUF_CR_LF_CR_LF = new Buffer([0xd, 0xa, 0xd, 0xa]), BUF_LF_LF = new Buffer([0xa, 0xa]);
+var STATE_NONE = 0, STATE_FOUND_LF = 1, STATE_FOUND_LF_CR = 2;
 
-        var buf_ary = [], unsavedStart = 0, buf_len = buf.length;
+function createPortForwarder(local_host, local_port, remote_host, remote_port, buf_proxy_basic_auth) {
+    net.createServer({allowHalfOpen: true}, function (socket) {
+        var realCon = net.connect({port: remote_port, host: remote_host, allowHalfOpen: true});
+        realCon.on('data', function (buf) {
+            //console.log('<<<<' + (t=new Date()) + '.' + t.getMilliseconds() + '\n' + buf.toString('ascii'));
+            socket.write(buf);
+        }).on('end', function () {
+            socket.end();
+        }).on('close', function () {
+            socket.end();
+        }).on('error', dummy);
 
-        //process orphan CR
-        if (state === STATE_FOUND_LF_CR && buf[0] !== LF) {
-            parser.execute(BUF_CR);
-            buf_ary.push(BUF_CR);
-        }
+        var parser = new HTTPParser(HTTPParser.REQUEST);
+        parser[HTTPParser.kOnHeadersComplete] = function () {
+            //console.log('---- kOnHeadersComplete----');
+            //console.log(arguments);
+            parser.__is_headers_complete = true;
+        };
+        //parser[HTTPParser.kOnMessageComplete] = function () {
+        //    console.log('---- kOnMessageComplete----');
+        //    console.log(arguments);
+        //};
 
-        for (var i = 0; i < buf_len; i++) {
-            //find first LF
-            if (state === STATE_NONE) {
-                if (buf[i] === LF) {
-                    state = STATE_FOUND_LF;
-                }
-                continue;
+        var state = STATE_NONE;
+
+        socket.on('data', function (buf) {
+            //console.log('>>>>' + (t = new Date()) + '.' + t.getMilliseconds() + '\n' + buf.toString('ascii'));
+            //var ret = parser.execute(buf);
+            //console.log('\n\n----parser result: ' + ret + ' buf len:' + buf.length);
+            //realCon.write(buf);
+            //return;
+
+            var buf_ary = [], unsavedStart = 0, buf_len = buf.length;
+
+            //process orphan CR
+            if (state === STATE_FOUND_LF_CR && buf[0] !== LF) {
+                parser.execute(BUF_CR);
+                buf_ary.push(BUF_CR);
             }
 
-            //find second CR LF or LF
-            if (buf[i] === LF) {
-                parser.__is_headers_complete = false;
-                parser.execute(buf.slice(unsavedStart, i + 1));
+            for (var i = 0; i < buf_len; i++) {
+                //find first LF
+                if (state === STATE_NONE) {
+                    if (buf[i] === LF) {
+                        state = STATE_FOUND_LF;
+                    }
+                    continue;
+                }
 
-                if (parser.__is_headers_complete) {
-                    //console.log('insert auth header');
-                    buf_ary.push(buf.slice(unsavedStart, buf[i - 1] === CR ? i - 1 : i));
-                    buf_ary.push(cfg.buf_proxy_basic_auth);
-                    buf_ary.push(state === STATE_FOUND_LF_CR ? BUF_CR_LF_CR_LF : BUF_LF_LF);
+                //find second CR LF or LF
+                if (buf[i] === LF) {
+                    parser.__is_headers_complete = false;
+                    parser.execute(buf.slice(unsavedStart, i + 1));
 
-                    unsavedStart = i + 1;
+                    if (parser.__is_headers_complete) {
+                        buf_ary.push(buf.slice(unsavedStart, buf[i - 1] === CR ? i - 1 : i));
+                        //console.log('insert auth header');
+                        buf_ary.push(buf_proxy_basic_auth);
+                        buf_ary.push(state === STATE_FOUND_LF_CR ? BUF_CR_LF_CR_LF : BUF_LF_LF);
+
+                        unsavedStart = i + 1;
+                        state = STATE_NONE;
+                    }
+                    else {
+                        state = STATE_FOUND_LF;
+                    }
+                }
+                else if (buf[i] === CR && state === STATE_FOUND_LF) {
+                    state = STATE_FOUND_LF_CR;
+                } else {
                     state = STATE_NONE;
                 }
-                else {
-                    state = STATE_FOUND_LF;
+            }
+
+            if (unsavedStart < buf_len) {
+                //strip last CR if found LF_CR
+                buf = buf.slice(unsavedStart, state === STATE_FOUND_LF_CR ? buf_len - 1 : buf_len);
+                if (buf.length) {
+                    parser.execute(buf);
+                    buf_ary.push(buf);
                 }
             }
-            else if (buf[i] === CR && state === STATE_FOUND_LF) {
-                state = STATE_FOUND_LF_CR;
-            } else {
-                state = STATE_NONE;
+
+            buf = Buffer.concat(buf_ary);
+            realCon.write(buf);
+
+        }).on('end', cleanup).on('close', cleanup).on('error', dummy);
+
+        function cleanup() {
+            if (parser) {
+                parser.close();
+                parser = null;
             }
+            realCon.end();
         }
+    }).on('error', function (err) {
+        console.log('Failed to listen at ' + local_host + ':' + local_port + '\n' + err);
+        process.exit(1);
+    }).listen(local_port, local_host === '*' ? undefined : local_host, function () {
+        console.log('OK: forward ' + local_host + ':' + local_port + ' to ' + remote_host + ':' + remote_port);
+    });
+}
 
-        if (unsavedStart < buf_len) {
-            //strip last CR if found LF_CR
-            buf = buf.slice(unsavedStart, state === STATE_FOUND_LF_CR ? buf_len - 1 : buf_len);
-            if (buf.length) {
-                parser.execute(buf);
-                buf_ary.push(buf);
-            }
-        }
+var proxyAddrMap = {};
+function createPacServer(local_host, local_port, remote_host, remote_port, buf_proxy_basic_auth, pac_url) {
+    var i = 0;
+    net.createServer({allowHalfOpen: true}, function (socket) {
+        var realCon = net.connect({port: remote_port, host: remote_host, allowHalfOpen: true});
 
-        buf = Buffer.concat(buf_ary);
-        realCon.write(buf);
+        var buf_ary = [];
+        realCon.on('data', function (buf) {
+            //console.log('<<<<' + (t=new Date()) + '.' + t.getMilliseconds() + '\n' + buf.toString('ascii'));
+            buf_ary.push(buf);
+        }).on('end', function () {
+            var s = Buffer.concat(buf_ary).toString();
+            buf_ary = [];
+            s = s.replace(/(['"])PROXY\s+([^'":\s]+)(:\d+)?\1/g, function (all, _q, _remote_host, _comma_remote_port) {
+                var _remote_port = _comma_remote_port && Number(_comma_remote_port.slice(1)) || 80;
+                var remoteAddr = _remote_host + ':' + _remote_port;
+                var _local_port = proxyAddrMap[remoteAddr];
+                if (!_local_port) {
+                    _local_port = local_port + (++i);
+                    proxyAddrMap[remoteAddr] = _local_port;
+                    createPortForwarder(local_host, _local_port, _remote_host, _remote_port, buf_proxy_basic_auth);
+                }
+                return _q + 'PROXY ' + local_host + ':' + _local_port + _q;
+            });
+            s = s.replace(/Content-Length:.*$/i, '');
+            console.log('return patched pac');
+            socket.end(s);
+        }).on('close', function () {
+            socket.end();
+        }).on('error', dummy);
 
-    }).on('end', cleanup).on('close', cleanup).on('error', dummy);
+        realCon.end('GET ' + pac_url + ' HTTP/1.1\r\n'
+            + 'Host: ' + local_host + ':' + local_port + '\r\n'
+            + 'Connection: keep-alive\r\n'
+            + 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36\r\n'
+            + 'Accept-Encoding: gzip, deflate\r\n'
+            + '\r\n');
 
-    function cleanup() {
-        if (parser) {
-            parser.close();
-            parser = null;
-        }
-        realCon.end();
-    }
-}).listen(cfg.port, cfg.host === '*' ? undefined : cfg.host);
+        socket.on('data', dummy).on('error', dummy);
+    }).on('error', function (err) {
+        console.log('Failed to listen at ' + local_host + ':' + local_port + '\n' + err);
+        process.exit(1);
+    }).listen(local_port, local_host === '*' ? undefined : local_host, function () {
+        console.log('OK: listen at ' + local_host + ':' + local_port);
+    });
+}
 
 function dummy() {
 }
+
+main();
